1659e1ec17342d89fc47884c6642109034479af1
ABUBUKKER CHAUDHARY <abubukkerchaudhary@ABUBUKKERs-MacBook-Air.local>
2021-08-09 15:40:55 -0400

    make it better

--- index.js
+++ index.js
@@ -5,12 +5,11 @@ const router = require("./routes/routes");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello brother</h1>");
 });
-app.use("/api", router);
+app.use("/", router);
 
 app.listen(PORT, console.log(`Server is Listening on port ${PORT}`));
